b4c90fb98ff45d9f96240cf86a43617f0e715410
3rfaan <28968837+3rfaan@users.noreply.github.com>
2022-12-01 13:17:12 +0100

    Refactoring plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,34 +10,34 @@
 
 -- PLUGINS
 require("paq")({
-    "savq/paq-nvim", -- Let Paq manage itself
-    "nvim-lualine/lualine.nvim", -- Status line
+	"savq/paq-nvim", -- Let Paq manage itself
+	"nvim-lualine/lualine.nvim", -- Status line
 
-    -- LSP
-    "neovim/nvim-lspconfig", -- LSP
-    "williamboman/mason.nvim", -- Manager for LSP, DAP, linters and formatters
-    'jose-elias-alvarez/null-ls.nvim'; -- Null server
+	-- LSP
+	"neovim/nvim-lspconfig", -- LSP
+	"williamboman/mason.nvim", -- Manager for LSP, DAP, linters and formatters
+	"jose-elias-alvarez/null-ls.nvim", -- Null server
 
-    "hrsh7th/nvim-cmp", -- Autocompletion plugin
-    "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-    "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
-    "L3MON4D3/LuaSnip", -- Snippets plugin
+	"hrsh7th/nvim-cmp", -- Autocompletion plugin
+	"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+	"saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+	"L3MON4D3/LuaSnip", -- Snippets plugin
 
-    "simrat39/rust-tools.nvim", -- Rust language tools
+	"simrat39/rust-tools.nvim", -- Rust language tools
 
-    "simrat39/inlay-hints.nvim", -- Inline hints
+	"simrat39/inlay-hints.nvim", -- Inline hints
 
-    -- Tools
-    "preservim/nerdtree", -- NerdTree
-    "ryanoasis/vim-devicons", -- Icons for NerdTree
-    "nvim-treesitter/nvim-treesitter", -- TreeSitter
-    "nvim-lua/plenary.nvim", -- Dependency of Telescope
-    "nvim-telescope/telescope.nvim", -- Telescope
-    "jiangmiao/auto-pairs", -- Auto Pairs
+	-- Tools
+	"preservim/nerdtree", -- NerdTree
+	"ryanoasis/vim-devicons", -- Icons for NerdTree
+	"nvim-treesitter/nvim-treesitter", -- TreeSitter
+	"nvim-lua/plenary.nvim", -- Dependency of Telescope
+	"nvim-telescope/telescope.nvim", -- Telescope
+	"jiangmiao/auto-pairs", -- Auto Pairs
 
-    -- Search
-    "junegunn/fzf", -- FZF
+	-- Search
+	"junegunn/fzf", -- FZF
 
-    -- Themes
-    "neanias/everforest-nvim", -- Everforest
+	-- Themes
+	"neanias/everforest-nvim", -- Everforest
 })
