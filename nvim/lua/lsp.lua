6130bc30a968382599f344fc5072bb7b752a5108
3rfaan <28968837+3rfaan@users.noreply.github.com>
2022-12-16 01:22:47 +0100

    Minor formatting

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -15,6 +15,7 @@ require("mason-lspconfig").setup()
 
 local lspconfig = require("lspconfig")
 local null_ls = require("null-ls")
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- LSP
