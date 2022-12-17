253b0ac21ff51d883b71fe58d109e9f299fcdc17
Arfan Zubi <arfan@MacBook-Air-von-Arfan.local>
2022-12-17 20:01:53 +0100

    Refactored init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,11 +9,11 @@
 -- Neovim Lua Config File by Arfan Zubi
 
 -- IMPORTS
+require("plugins") -- Plugins
+require("setups") -- Setup of plugins
+
 require("settings") -- Editor settings
 require("mappings") -- Mappings
 require("scripts") -- Scripts
-
-require("plugins") -- Plugins
-require("setups") -- Setup of plugins
 
 require("lsp-config") -- LSP mappings and settings
