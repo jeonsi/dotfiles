10508e02ffe60d8f04f6772eed883ef72a3713fc
3rfaan <28968837+3rfaan@users.noreply.github.com>
2022-12-21 19:02:36 +0100

    Modified i3 config and bash script for desktop wallpaper in multiple monitors

--- asus/Xorg/toggle-external-monitor.sh
+++ asus/Xorg/toggle-external-monitor.sh
@@ -6,4 +6,5 @@ if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
 else
     xrandr --output "$intern" --primary --auto --output "$extern" --above "$intern" --auto
+    feh --bg-center $HOME/Downloads/flowers.png $HOME/Downloads/flowers.png
 fi
