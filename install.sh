e8d09b06b67a415c4045a88d9e06b5e6d4711eba
3rfaan <28968837+3rfaan@users.noreply.github.com>
2023-01-09 21:34:50 +0100

    Using vars for directories

--- install.sh
+++ install.sh
@@ -8,50 +8,82 @@ grn=$(tput setaf 2)
 ylw=$(tput setaf 3)
 gry=$(tput setaf 8)
 
-printf "${gry}%s${normal}\n${bold}${grn}%s${normal}\n${ylw}%s${normal}\n\n" "
- █████╗ ██████╗  ██████╗██╗  ██╗    ███████╗ ██████╗ ██████╗ ███████╗███████╗████████╗
-██╔══██╗██╔══██╗██╔════╝██║  ██║    ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝╚══██╔══╝
-███████║██████╔╝██║     ███████║    █████╗  ██║   ██║██████╔╝█████╗  ███████╗   ██║   
-██╔══██║██╔══██╗██║     ██╔══██║    ██╔══╝  ██║   ██║██╔══██╗██╔══╝  ╚════██║   ██║   
-██║  ██║██║  ██║╚██████╗██║  ██║    ██║     ╚██████╔╝██║  ██║███████╗███████║   ██║   
-╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   
-" \
-	"ARCH FOREST INSTALLER Version 1.0" \
-	"Everforest theme for Arch Linux by Arfan Zubi"
+CONFIG_DIR=$HOME/.config
+DOWNLOADS=$XDG_DOWNLOAD_DIR
 
+printf "${gry}%s${normal}\n${bold}${grn}%s${normal}\n${ylw}%s${normal}\n\n" "
+
+███████╗██╗   ██╗███████╗██████╗ ███████╗ ██████╗ ██████╗ ███████╗███████╗████████╗
+██╔════╝██║   ██║██╔════╝██╔══██╗██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝╚══██╔══╝
+█████╗  ██║   ██║█████╗  ██████╔╝█████╗  ██║   ██║██████╔╝█████╗  ███████╗   ██║   
+██╔══╝  ╚██╗ ██╔╝██╔══╝  ██╔══██╗██╔══╝  ██║   ██║██╔══██╗██╔══╝  ╚════██║   ██║   
+███████╗ ╚████╔╝ ███████╗██║  ██║██║     ╚██████╔╝██║  ██║███████╗███████║   ██║   
+╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   
+
+" \
+	"EVERFOREST THEME FOR ARCH LINUX Version 1.0" \
+	"Author: Arfan Zubi" \
+    "License: GNU General Public License"
+
+# Installer
 function install() {
-	if [[ ! -d $HOME/.config ]]; then
-		mkdir -p "$HOME/.config"
-	else
-		rm -rf "$HOME/.config"
+	if [[ -d "$CONFIG_DIR" ]]; then
+		echo "${ylw}$CONFIG_DIR does already exist, deleting all files...${normal}"
+		rm -rfv "$HOME/.config"
 	fi
 
-    xdg-user-dirs-update
+	echo "${ylw}Creating new $CONFIG_DIR directory...${normal}"
+	mkdir -p "$HOME/.config"
 
-	if [[ -d $XDG_DOWNLOAD_DIR/dotfiles ]]; then
-		cp -r -f "$XDG_DOWNLOAD_DIR/dotfiles/*" "$HOME/.config"
+	echo "${ylw}Creating XDG user directories...${normal}"
+	xdg-user-dirs-update
+	echo "${ylw}Done!${normal}"
+
+	if [[ -d $DOWNLOADS ]]; then
+        echo "${ylw}Copying files from ${DOWNLOADS} to ${CONFIG_DIR}${normal}"
+		cp -a "$DOWNLOADS/dotfiles/." "$HOME/.config"
+        cp -a "$DOWNLOADS/dotfiles/Xorg/." "/etc/X11/xorg.conf.d"
 
 		cd "$HOME/.config" || exit 1
-        echo "${ylw}Changed directory to $(pwd)${normal}"
+		echo "${ylw}Changed directory to $(pwd)${normal}"
 
 		echo "${ylw}Removing some unnecessary files...${normal}"
-		rm -rfv asus Xorg urxvt i3/i3status README.md screenshot-20221230-145517Z-all.png nvim/init.vim nvim/coc-settings.json
+		rm -rfv asus Xorg urxvt i3/i3status screenshot-20221230-145517Z-all.png nvim/init.vim nvim/coc-settings.json install.sh
 
 		echo "${ylw}Installing shell tools${normal}"
+		git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+		git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions"
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh/zsh-syntax-highlighting"
 		git clone https://github.com/alexanderjeurissen/ranger_devicons "$HOME/.config/ranger/plugins/ranger_devicons"
-    else
-        echo "${bold}${red}dotfiles directory not found in your Downloads folder! Make sure you have xdg-user-dirs installed and run xdg-user-dirs-update!${normal}"
-        exit 1
+
+		git clone https://github.com/sebastiencs/icons-in-terminal.git "/usr/share"
+
+		if [[ -d /usr/share/icons-in-terminal ]]; then
+			sudo /usr/share/icons-in-terminal/install.sh
+		fi
+
+	else
+        echo "${bold}${red}dotfiles directory not found in your Downloads folder! Make sure you cloned the dotfiles (git clone https://github.com/3rfaan/dotfiles.git) and you have xdg-user-dirs installed and run xdg-user-dirs-update!${normal}"
+		exit 1
+	fi
+}
+
+# Removing install files after moving them to .config
+function rm_install_file() {
+	if [[ -d $DOWNLOADS/dotfiles ]]; then
+		echo "${ylw}Removing installation files...${normal}"
+		rm -rf "$DOWNLOADS/dotfiles"
 	fi
 }
 
 while true; do
 	echo -e "${bold}${red}===> This installer will delete ALL files in your ~/.config directory.${normal}"
-	read -r -p "${ylw}Do you want to proceed? (yes/no) ${normal}" yn
+	read -r -p "${ylw}Do you want to proceed? [y/N] ${normal}" yn
 
 	case $yn in
 	[Yy]*)
 		install
+		rm_install_file
 		exit 0
 		;;
 	[Nn]*)
