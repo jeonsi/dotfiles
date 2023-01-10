d58224f2217f09edd5143dcd6b29b4a2cf405e3c
3rfaan <28968837+3rfaan@users.noreply.github.com>
2023-01-10 23:39:03 +0100

    Added fallbacks

--- install.sh
+++ install.sh
@@ -27,36 +27,67 @@ printf "${gry}%s${normal}\n${bold}${grn}%s${normal}\n${ylw}%s${normal}\n${gry}%s
 
 # Installer
 function install() {
+	# Checking if ~/.config already exists and wipe it if it does
 	if [[ -d "$CONFIG_DIR" ]]; then
 		echo "${ylw}$CONFIG_DIR does already exist, deleting all files...${normal}"
 		rm -rfv "$CONFIG_DIR"
 	fi
 
+	# Creating new empty ~/.config
 	echo "${ylw}Creating new $CONFIG_DIR directory...${normal}"
-	mkdir -p "$CONFIG_DIR"
-
-	echo "${ylw}Creating XDG user directories...${normal}"
-	xdg-user-dirs-update
+	mkdir -p "$CONFIG_DIR" || {
+		echo "${bold}${red}Failed creating ${CONFIG_DIR}, exiting...${normal}"
+		exit 1
+	}
 	echo "${ylw}Done!${normal}"
 
-	if [[ -d $DOWNLOAD_DIR ]]; then
-		echo "${ylw}Copying files from ${DOWNLOAD_DIR} to ${CONFIG_DIR}${normal}"
-		cp -av "$DOWNLOAD_DIR/dotfiles/." "$CONFIG_DIR"
+	# Refreshing XDG user directories
+	echo "${ylw}Creating XDG user directories...${normal}"
+	xdg-user-dirs-update || {
+		echo "${bold}${red}Failed creating XDG user directories, exiting...${normal}"
+		exit 1
+	}
+	echo "${ylw}Done!${normal}"
 
+	# Checking if dotfiles are in Downloads directory
+	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
+
+		# Removing .git directory so it won't get copied unnecessary
+		echo "${ylw}Removing .git directory...${normal}"
+		rm -rf .git || {
+			echo "${bold}${red}Failed removing .git directory, exiting...${normal}"
+			exit 1
+		}
+		echo "${ylw}Done!${normal}"
+
+		# Copying dotfiles from Downloads folder to ~/.config
+		echo "${ylw}Copying files from ${DOWNLOAD_DIR} to ${CONFIG_DIR}...${normal}"
+		cp -Rfv "$DOWNLOAD_DIR/dotfiles/." "$CONFIG_DIR" || {
+			echo "${bold}${red}Failed copying files from ${DOWNLOAD_DIR} to ${CONFIG_DIR}, exiting...${normal}"
+			exit 1
+		}
+		echo "${ylw}Done!${normal}"
+
+		# Copying files which target destinations are not in ~/.config
 		echo "${ylw}Copying files to other directories${normal}"
-		sudo cp -av "$DOWNLOAD_DIR/dotfiles/Xorg/." "/etc/X11/xorg.conf.d"
+		sudo cp -Rfv "$DOWNLOAD_DIR/dotfiles/Xorg/." "/etc/X11/xorg.conf.d"
 		if [[ ! -d "$HOME/.local/share/rofi" ]]; then
 			mkdir -p "$HOME/.local/share/rofi/themes"
-			cp -av "$DOWNLOAD_DIR/dotfiles/rofi/squared-everforest.rasi" "$HOME/.local/share/rofi/themes/squared-everforest.rasi"
+			cp -Rfv "$DOWNLOAD_DIR/dotfiles/rofi/squared-everforest.rasi" "$HOME/.local/share/rofi/themes/squared-everforest.rasi"
 		fi
-        cp -av "$DOWNLOAD_DIR/dotfiles/Xorg/.xinitrc" "$HOME/.xinitrc"
-		cp -av "$DOWNLOAD_DIR/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+		cp -Rfv "$DOWNLOAD_DIR/dotfiles/Xorg/.xinitrc" "$HOME/.xinitrc"
+		cp -Rfv "$DOWNLOAD_DIR/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+		echo "${ylw}Done!${normal}"
 
 		cd "$CONFIG_DIR" || exit 1
 		echo "${ylw}Changed directory to $(pwd)${normal}"
 
 		echo "${ylw}Removing some unnecessary files...${normal}"
-		rm -rfv asus Xorg urxvt lf i3/i3status screenshot-20221230-145517Z-all.png nvim/init.vim nvim/coc-settings.json install.sh zsh
+		rm -rfv asus Xorg urxvt lf i3/i3status screenshot-20221230-145517Z-all.png nvim/init.vim nvim/coc-settings.json install.sh zsh || {
+			echo "${bold}${red}Failed removing directories not needed for the configuration, exiting..."
+			exit 1
+		}
+		echo "${ylw}Done!${normal}"
 
 		echo "${ylw}Installing shell tools${normal}"
 		git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
@@ -70,6 +101,7 @@ function install() {
 			cd "/usr/share/icons-in-terminal" || exit 1
 			/usr/share/icons-in-terminal/install.sh
 		fi
+		echo "${ylw}Done!${normal}"
 
 	else
 		echo "${bold}${red}Make sure you cloned (https://github.com/3rfaan/dotfiles.git) into your ~/Downloads folder!${normal}"
@@ -98,7 +130,7 @@ while true; do
 		exit 0
 		;;
 	[Nn]* | "")
-		echo "Aborting installation..."
+		echo "${bold}${red}Aborting installation...${normal}"
 		exit 1
 		;;
 	*) echo "Please enter [y]es or [n]o!" ;;
