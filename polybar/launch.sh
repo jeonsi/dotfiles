91ac9b6d9d9424e2cb1bb9aa7f513c20f8551107
3rfaan <28968837+3rfaan@users.noreply.github.com>
2023-03-07 00:59:05 +0100

    Deleted polybar in asus directory as they are similar, reformatted launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar everforest 2>&1 | tee -a /tmp/polybar.log & disown
+# Launch bars on multiple displays
+for m in $(polybar -M | cut -d ':' -f 1); do
+    MONITOR=$m polybar --reload everforest &
+done
 
-echo "Polybar launched..."
-
+echo "Bars launched..."
