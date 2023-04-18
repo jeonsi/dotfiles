fcbb70bd5f6a80afaa09a75b534aa3da61cc089f
3rfaan <28968837+3rfaan@users.noreply.github.com>
2023-04-19 00:01:05 +0200

    Disabled default COQ keymap for <C-h> as it interferes with changing active buffer keymap

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -125,3 +125,10 @@ g.loaded_python3_provider = 0
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_node_provider = 0
+
+-- Disabling default COQ keymap for <C-h> as it interferes with keymappings for changing active buffer
+g.coq_settings = {
+	keymap = {
+		jump_to_mark = "null",
+	},
+}
